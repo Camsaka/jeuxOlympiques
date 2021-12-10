80c1153704c60655a55132028bf139b835c63642
Camille Gautier <camille.gaut@sfr.fr>
2021-12-10 16:47:11 +0100

    add boutons listes

--- src/public/scriptFront.js
+++ src/public/scriptFront.js
@@ -1,4 +1,23 @@
+
+
 let conteneurPrincipal = document.getElementById("conteneur");
-let liste = document.createElement("li");
-liste.textContent = "test";
-conteneurPrincipal.appendChild(liste);
+let boutonListSports = document.getElementById("listSportsButton");
+let boutonListAthletes = document.getElementById("listAthletesButton");
+
+
+
+async function listerSports(){
+    let listeSports = document.createElement("ul");
+    const response = await fetch('http://localhost:3001/api/sports');
+    const data = await response.json();
+    console.log(data);
+    for (let elt of data.sports){
+        let point = document.createElement("li");
+        point.textContent = elt.name;
+        listeSports.appendChild(point);
+        console.log(elt.name);
+    }
+    conteneurPrincipal.appendChild(listeSports);
+}
+
+
